597cfdf053abff641ae487c69343d0373b7706ff
roksan <glehdtm@hanmail.net>
2026-02-10 21:10:33 +0900

    누락 코드 추가

--- Assets/_Core/Scripts/Target/Character/Worker/Weapon/Weapon_Ranged.cs
+++ Assets/_Core/Scripts/Target/Character/Worker/Weapon/Weapon_Ranged.cs
@@ -1,16 +1,5 @@
 using UnityEngine;
 
-public class Weapon_Projecttile : MonoBehaviour
+public class Weapon_Ranged : Character_Weapon
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
